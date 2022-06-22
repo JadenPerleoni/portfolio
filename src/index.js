a320a29507144325c35c2c507737d8cc5b925b95
JadenPerleoni <Jadenperleoni@gmail.com>
2022-06-22 09:57:46 -0700

    Laptop commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const Container = () => {
         {
             id: 3, 
             skill: 'React',
-            skillInfo: `I started learning reacting to gain knowledge on front-end development.
+            skillInfo: `I started learning reacting to git gain knowledge on front-end development.
             While I greatly prefer back-end, I feel that it's important to learn fullstack development.`
         },
     ]
